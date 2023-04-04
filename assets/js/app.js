62b9aa5c025f2de8b9244329aa1614de2b4fbb2f
Milad Zahmatkesh <mizahma@siue.edu>
2023-04-04 08:38:57 -0500

    fix reset password feature

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,18 @@
 function sendEmail(email) {
 
-    // check is the email valid?
-    if(email== "mizahma@siue.edu"){
+    // check if the email is valid?
+    if(email=== "admin@gamex.com"){
         console.log("Email send!");
         return true;
     }
     return false;
 }
 
-
 function checkEmail(e) {
     e.preventDefault();
     //get value email input
     const email= document.querySelector(".email").value 
-    const result= sendEmail(email);//result is value that backend returns
+    const result= sendEmail(email);//result is a value that backend returns
     if (!result) {
         document.querySelector(".error").innerText="Email is not correct!";
     } else {
@@ -29,4 +28,53 @@ function checkEmail(e) {
     
 }
 
-document.querySelector(".forgot-password-form").addEventListener("submit",checkEmail)
+
+function checkOldPassword(password) {
+    // check if the password is valid?
+    if(password === "admin"){
+        return true;
+    }
+    return false;
+}
+
+function resetPassword(e) {
+    e.preventDefault();
+    const oldPassword= document.querySelector(".old-password").value;
+    const newPassword= document.querySelector(".new-password").value;
+    const rePassword= document.querySelector(".re-password").value;
+
+    //result is a value that backend returns
+    const result= checkOldPassword(oldPassword);
+    if (!result) {
+        document.querySelector(".old-password-error").innerText="Old password is not correct!";
+        return;
+    }
+    else{
+        document.querySelector(".old-password-error").innerText="";
+    }
+    if (newPassword!==rePassword) {
+        document.querySelector(".re-password-error").innerText="Passwords do not match!";
+        return;
+    }else{
+        document.querySelector(".old-password-error").innerText="";
+    }
+
+    document.querySelector(".form-container").innerHTML=`
+    <div class="send-email">
+         <h1 class="success">
+            Your Password changed successfully.
+         </h1>
+         <a href="index.html">Back To Dashboard</a>
+     </div>
+    `
+
+}
+
+const forgotPasswordForm=document.querySelector(".forgot-password-form")
+if (forgotPasswordForm) {
+    forgotPasswordForm.addEventListener("submit",checkEmail)
+}
+const resetPasswordForm=document.querySelector(".reset-password-form")
+if (resetPasswordForm) {
+    resetPasswordForm.addEventListener("submit",resetPassword)
+}
